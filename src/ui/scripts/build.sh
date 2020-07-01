7cc4f4f560773f455d65e4a9855e70c9c8e49ecf
Niall Thomson <thomson.niall@gmail.com>
2020-07-01 15:03:09 -0700

    Added scripts for ui component

---
+++ src/ui/scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "$0")
+CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
+
+if [ -z "$TAG" ]; then
+  TAG="latest"
+fi
+
+if [ -z "$REPO" ]; then
+  REPO="microservices-demo"
+fi
+
+docker build -f $CODE_DIR/../../images/java11/Dockerfile --build-arg JAR_PATH=target/ui-0.0.1-SNAPSHOT.jar \
+  -t $REPO/shop-ui:$TAG $CODE_DIR
