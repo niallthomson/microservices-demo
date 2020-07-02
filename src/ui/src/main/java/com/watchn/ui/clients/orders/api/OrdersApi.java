b2bba27953d22f1f28cfc6d2576ee3613e91e0b1
Niall Thomson <thomson.niall@gmail.com>
2020-07-01 20:22:00 -0700

    Added order service to ui

---
+++ src/ui/src/main/java/com/watchn/ui/clients/orders/api/OrdersApi.java
@@ -0,0 +1,89 @@
+package com.watchn.ui.clients.orders.api;
+
+import com.watchn.ui.clients.orders.ApiClient;
+
+import com.watchn.ui.clients.orders.model.CreateOrderRequest;
+import com.watchn.ui.clients.orders.model.CreateOrderResponse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-07-01T16:06:42.716331-07:00[America/Los_Angeles]")
+public class OrdersApi {
+    private ApiClient apiClient;
+
+    public OrdersApi() {
+        this(new ApiClient());
+    }
+
+    @Autowired
+    public OrdersApi(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public ApiClient getApiClient() {
+        return apiClient;
+    }
+
+    public void setApiClient(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    /**
+     * Create an order
+     * 
+     * <p><b>200</b> - OK
+     * <p><b>201</b> - Created
+     * <p><b>401</b> - Unauthorized
+     * <p><b>403</b> - Forbidden
+     * <p><b>404</b> - Not Found
+     * @param createOrderRequest createOrderRequest
+     * @return CreateOrderResponse
+     * @throws RestClientException if an error occurs while attempting to invoke the API
+     */
+    public Mono<CreateOrderResponse> createOrder(CreateOrderRequest createOrderRequest) throws RestClientException {
+        Object postBody = createOrderRequest;
+        // verify the required parameter 'createOrderRequest' is set
+        if (createOrderRequest == null) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'createOrderRequest' when calling createOrder");
+        }
+        // create path and map variables
+        final Map<String, Object> pathParams = new HashMap<String, Object>();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+        final HttpHeaders headerParams = new HttpHeaders();
+        final MultiValueMap<String, String> cookieParams = new LinkedMultiValueMap<String, String>();
+        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+
+        final String[] localVarAccepts = { 
+            "*/*"
+        };
+        final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        final String[] localVarContentTypes = { 
+            "application/json"
+        };
+        final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+        String[] localVarAuthNames = new String[] {  };
+
+        ParameterizedTypeReference<CreateOrderResponse> localVarReturnType = new ParameterizedTypeReference<CreateOrderResponse>() {};
+        return apiClient.invokeAPI("/orders", HttpMethod.POST, pathParams, queryParams, postBody, headerParams, cookieParams, formParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+    }
+}
