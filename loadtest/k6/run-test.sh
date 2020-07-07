31a3d61be7f40d631c96d7cf216492e13028d100
Niall Thomson <thomson.niall@gmail.com>
2020-07-06 21:26:59 -0700

    Pass target param through compose

--- loadtest/k6/run-test.sh
+++ loadtest/k6/run-test.sh
@@ -6,6 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 base_url=$1
 region=$2
+target=$3
 
 if [ -z "$base_url" ]; then
   echo "Error: No base URL specified - ./run-test.sh <baseurl> <region>"
@@ -17,10 +18,16 @@ if [ -z "$region" ]; then
   exit 1
 fi
 
+if [ -z "$target" ]; then
+  echo "Defaulting target to 20"
+  target="20"
+fi
+
 cd $DIR
 
 docker-compose run -v \
     $PWD/scripts:/scripts \
     -e WATCHN_BASE_URL=$base_url \
     -e WATCHN_REGION=$region \
+    -e WATCHN_TARGET=$target \
     k6 run /scripts/script.js
