c716da9031bae469d2fd089c740e909e13863d18
Niall Thomson <thomson.niall@gmail.com>
2020-07-06 20:48:32 -0700

    Added posting forms to cart and checkout, region awareness

--- loadtest/k6/run-test.sh
+++ loadtest/k6/run-test.sh
@@ -5,9 +5,15 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 base_url=$1
+region=$2
 
 if [ -z "$base_url" ]; then
-  echo "Error: Not base URL specified - ./run-test.sh <baseurl>"
+  echo "Error: No base URL specified - ./run-test.sh <baseurl> <region>"
+  exit 1
+fi
+
+if [ -z "$region" ]; then
+  echo "Error: No region specified - ./run-test.sh <baseurl> <region>"
   exit 1
 fi
 
@@ -16,4 +22,5 @@ cd $DIR
 docker-compose run -v \
     $PWD/scripts:/scripts \
     -e WATCHN_BASE_URL=$base_url \
+    -e WATCHN_REGION=$region \
     k6 run /scripts/script.js
