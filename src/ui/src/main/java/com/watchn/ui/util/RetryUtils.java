d6da1b69690f55369a7779430148847f7a61a038
Niall Thomson <thomson.niall@gmail.com>
2020-07-28 10:27:06 -0700

    Started filtering errors that will be retried

--- src/ui/src/main/java/com/watchn/ui/util/RetryUtils.java
+++ src/ui/src/main/java/com/watchn/ui/util/RetryUtils.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.util.retry.Retry;
 import reactor.util.retry.RetryBackoffSpec;
 
+import java.net.ConnectException;
 import java.time.Duration;
 
 @Slf4j
@@ -11,6 +12,7 @@ public class RetryUtils {
     public static RetryBackoffSpec apiClientRetrySpec(String description) {
         return Retry
                 .backoff(3, Duration.ofSeconds(1))
+                .filter(throwable -> throwable instanceof ConnectException)
                 .doBeforeRetry(context -> log.warn("Retrying {}", description));
     }
 }
