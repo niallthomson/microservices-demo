6617d16a9cf6c502beb00f6713c1e2913a46a566
Niall Thomson <thomson.niall@gmail.com>
2020-09-22 11:33:45 -0700

    Further temporary fix for metadata bean conditionals

--- src/ui/src/main/java/com/watchn/ui/config/metadata/DefaultMetadataConfiguration.java
+++ src/ui/src/main/java/com/watchn/ui/config/metadata/DefaultMetadataConfiguration.java
@@ -2,6 +2,7 @@ package com.watchn.ui.config.metadata;
 
 import com.watchn.ui.services.Metadata;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.aws.context.annotation.ConditionalOnMissingAwsCloudEnvironment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class DefaultMetadataConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean(Metadata.class)
+    @ConditionalOnMissingAwsCloudEnvironment
     public Metadata metadata() {
         return new Metadata().add("environment", "local");
     }
