ba4c8b7f128bd7aa3cd68504fec3a4829d4c6061
Niall Thomson <thomson.niall@gmail.com>
2020-10-10 13:23:39 -0700

    Fix patch image script to only patch first occurence of tag

--- scripts/patch-image-versions.sh
+++ scripts/patch-image-versions.sh
@@ -12,7 +12,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for i in $DIR/../src/**/chart/values.yaml; do
     # Could use yq here but it strips whitespace
     # https://github.com/mikefarah/yq/issues/465
-    sed -ri "s/^(\s*)(tag:.*$)/\1tag: \"$TAG\"/" $i
+    sed -ri "0,/tag/{s/^(\s*)(tag:.*$)/\1tag: \"$TAG\"/}" $i
 done
 
 cat << EOF > $DIR/../deploy/terraform/modules/ecs-base/vars-tag.tf
