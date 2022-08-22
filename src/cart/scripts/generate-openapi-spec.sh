0c183f87f97b8a4de7b5383d040f18ded5970c4a
Niall Thomson <thomson.niall@gmail.com>
2022-08-21 22:12:04 -0600

    Reworked scripts for generating OpenAPI specs for Java services

--- src/cart/scripts/generate-openapi-spec.sh
+++ src/cart/scripts/generate-openapi-spec.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 SCRIPT_DIR=$(dirname "$0")
 
+export AWS_ACCESS_KEY_ID="dummy"
+export AWS_SECRET_ACCESS_KEY="dummy"
+
 (cd $SCRIPT_DIR/.. && ./mvnw verify)
 
 yq $SCRIPT_DIR/../target/openapi.json -P > $SCRIPT_DIR/../openapi.yml
